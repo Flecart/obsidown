5573511fab6d3adb20331192d27ed19df3fce3b3
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2024-03-10 20:11:53 +0100

    feat: add image conversion

--- obsidian_jekyll/__init__.py
+++ obsidian_jekyll/__init__.py
@@ -1,11 +1,31 @@
 import re
 
-def convert_maths(page):
+def convert_maths(page: str):
+    """ Convert single dollar sign math expressions to double dollar sign expressions.
+    Add newline characters before and after double dollar sign expressions if they are not already present.
+    
+    """
     # Replace single dollar sign math expressions with double dollar sign expressions
     page = re.sub(r'(?<!\$)\$([^\$]+?)\$(?!\$)', r'$$\1$$', page)
 
     # Add newline characters before and after double dollar sign expressions if they are not already present
     page = re.sub(r'\$\$\n([^\$]+?)\n\$\$', r'\n$$\n\1\n$$\n', page, flags=re.DOTALL)
+
+    return page
+
+def convert_images(page: str, base: str = ""):
+    """ Convert image obsidian markdown tags into html tags. 
+    Args
+    ----
+    page: str
+        The page content to be converted.
+    base: str
+        The base url for the images.
+    """
+    # First convert image tags with numbers to html image tags
+    page = re.sub(r'!\[\[([^\]]+?)(\.jpeg|\.webm|\.png)\|([0-9|\s]+)\]\]', r'<img src="' + base + '/' + r'\1\2" width="\3" alt="\1">', page)
+    # Convert markdown image tags to html image tags
+    page = re.sub(r'!\[\[([^\]]+?)(\.jpeg|\.webm|\.png)\]\]', r'<img src="' + base + '/' + r'\1\2" alt="\1">', page)
 
     return page
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from obsidian_jekyll import convert_maths
+from obsidian_jekyll import convert_maths, convert_images
 
 def test_convert_maths():
     # Test case: Single math expression
@@ -31,3 +31,26 @@ def test_convert_maths():
     page = "$$\nx = y\n$$ and $a = b$"
     expected_output = "\n$$\nx = y\n$$\n and $$a = b$$"
     assert convert_maths(page) == expected_output
+
+def test_convert_images():
+    base_url = "http://example.com/images"
+
+    # Test case: Single image tag
+    page = '![[image.png]]'
+    expected_output = '<img src="http://example.com/images/image.png" alt="image">'
+    assert convert_images(page, base_url) == expected_output
+
+    # Test case: Multiple image tags
+    page = '![[image1.jpeg]] and ![[image2.webm]]'
+    expected_output = '<img src="http://example.com/images/image1.jpeg" alt="image1"> and <img src="http://example.com/images/image2.webm" alt="image2">'
+    assert convert_images(page, base_url) == expected_output
+
+    # Test case: No image tags
+    page = "No images here"
+    expected_output = "No images here"
+    assert convert_images(page, base_url) == expected_output
+
+    # Test case: Image tag with number
+    page = '![[name.png|123]]'
+    expected_output = '<img src="http://example.com/images/name.png" width="123" alt="name">'
+    assert convert_images(page, base_url) == expected_output
